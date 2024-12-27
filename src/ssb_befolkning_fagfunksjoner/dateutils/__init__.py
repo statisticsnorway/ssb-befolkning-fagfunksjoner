5907b1758ed443604a4e1bacc7ff9564bc3dce8d
snergaard <grd@ssb.no>
2024-12-27 12:31:23 +0000

    fixing relative imports

---
+++ src/ssb_befolkning_fagfunksjoner/dateutils/__init__.py
@@ -0,0 +1,27 @@
+"""This module contains functionality around dates for statistics production in S320."""
+
+from .core import (
+    get_period_dates,
+    get_etterslep_dates,
+    get_period_label,
+)
+
+from .date_tools import (
+    get_next_period,
+    get_last_period,
+    get_last_day_of_next_month,
+    get_last_day_of_month,
+)
+
+from input_date_utils import get_user_inputs
+
+__all__ = [
+    "get_period_dates",
+    "get_etterslep_dates",
+    "get_period_label",
+    "get_next_period",
+    "get_last_period",
+    "get_last_day_of_next_month",
+    "get_last_day_of_month",
+    "get_user_inputs",
+]
