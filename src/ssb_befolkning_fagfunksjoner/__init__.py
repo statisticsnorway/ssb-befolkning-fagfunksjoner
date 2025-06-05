f0766cafdcdf298bdb58e99d933f9ee6f28a39d8
snergaard <grd@ssb.no>
2025-06-05 11:43:00 +0000

    format change

--- src/ssb_befolkning_fagfunksjoner/__init__.py
+++ src/ssb_befolkning_fagfunksjoner/__init__.py
@@ -1,7 +1,15 @@
 """Befolkning Fagfunksjoner."""
 
-from ssb_befolkning_fagfunksjoner.versions.versions import get_next_version_number, write_versioned_pandas
-from ssb_befolkning_fagfunksjoner.date_utils.date_utils import get_date_parameters, get_period_labels, get_last_day_of_month, get_last_day_of_next_month
+from ssb_befolkning_fagfunksjoner.versions.versions import (
+    get_next_version_number,
+    write_versioned_pandas,
+)
+from ssb_befolkning_fagfunksjoner.date_utils.date_utils import (
+    get_date_parameters,
+    get_period_labels,
+    get_last_day_of_month,
+    get_last_day_of_next_month,
+)
 
 __all__ = [
     "get_next_version_number",
@@ -9,5 +17,5 @@ __all__ = [
     "get_date_parameters",
     "get_period_labels",
     "get_last_day_of_month",
-    "get_last_day_of_next_month"
+    "get_last_day_of_next_month",
 ]
