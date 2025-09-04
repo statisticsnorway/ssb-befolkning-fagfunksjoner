e339b35a2d97b3dfb36f54a2a17769a9a089264c
Bjørn Lie Rapp <90246213+Bjoern-Rapp@users.noreply.github.com>
2025-09-04 10:40:35 +0200

    Added new graph based implementation "get_changes_mapping"

--- src/ssb_befolkning_fagfunksjoner/klass_utils/loaders.py
+++ src/ssb_befolkning_fagfunksjoner/klass_utils/loaders.py
@@ -9,9 +9,17 @@ Includes support for:
 - verdensinndeling
 """
 
+import datetime
+import functools
+from itertools import chain, pairwise, product
+from typing import Counter, NamedTuple, cast
+
+import networkx
 import pandas as pd
-from klass import KlassClassification
+from klass import KlassClassification, KlassVersion
 from klass import KlassCorrespondence
+from klass.requests.klass_types import CorrespondenceTablesType, VersionPartType
+
 
 # KLASS classification and correspondence IDs
 FYLKE_ID = "104"
@@ -86,31 +94,38 @@ def load_kommnr(reference_date: str) -> dict[str, str]:
 
 
 def load_kommnr_changes(
-    to_date: str, from_date: str = "1980-01-01"
+    to_date: str | datetime.date | None = None,
+    from_date: str | datetime.date = datetime.date(1980, 1, 1),
+    target_date: str | datetime.date | None = None
 ) -> tuple[pd.DataFrame, pd.DataFrame]:
     """Load KLASS changes for municipalities."""
     # Read kommnr changes from KLASS
-    kommnr_changes: pd.DataFrame = (
-        KlassClassification(KOMMUNE_ID)
-        .get_changes(from_date=from_date, to_date=to_date)[
-            ["oldCode", "newCode", "changeOccurred"]
-        ]
-        .sort_values("changeOccurred", ascending=True)
+    if isinstance(to_date, str):
+        to_date = datetime.date.fromisoformat(to_date)
+
+    if isinstance(from_date, str):
+        from_date = datetime.date.fromisoformat(from_date)
+
+    if isinstance(target_date, str):
+        target_date = datetime.date.fromisoformat(target_date)
+
+    if target_date is None and to_date is not None:
+        target_date = to_date
+
+    classification = KlassClassification(131)
+    kommnr_changes = get_changes_mapping(
+        classification, target_date=target_date, to_date=to_date, from_date=from_date
     )
 
     # Drop non-changes
-    kommnr_changes = kommnr_changes[
-        kommnr_changes["oldCode"] != kommnr_changes["newCode"]
-    ]
+    kommnr_changes = kommnr_changes.loc[kommnr_changes.index != kommnr_changes]
 
     # Identify splits and drop them from kommnr changes
-    kommnr_splits = kommnr_changes.loc[
-        kommnr_changes.duplicated(subset=["oldCode", "changeOccurred"], keep=False)
-    ]
-    kommnr_changes = kommnr_changes.loc[
-        ~kommnr_changes["oldCode"].isin(kommnr_splits["oldCode"].drop_duplicates()),
-        ["oldCode", "newCode"],
-    ]
+    mask = kommnr_changes.index.duplicated(keep=False)
+
+    kommnr_splits = kommnr_changes.loc[mask].reset_index()
+
+    kommnr_changes = kommnr_changes.loc[~mask].reset_index()
 
     return kommnr_changes, kommnr_splits
 
@@ -180,3 +195,167 @@ def load_verdensinndeling(reference_date: str) -> dict[str, str]:
             world_div_dict[value] = "4"
 
     return world_div_dict
+
+
+class _CodePoint(NamedTuple):
+    code: str
+    version: int
+
+
+def _get_from_date(versjon: VersionPartType) -> datetime.date:
+    return datetime.date.fromisoformat(versjon["validFrom"])
+
+
+def _get_to_date(versjon: VersionPartType) -> datetime.date | None:
+    try:
+        return datetime.date.fromisoformat(versjon["validTo"])
+    except KeyError:
+        return None
+
+
+def _dates_overlaps(
+    versjon: VersionPartType,
+    from_date: datetime.date,
+    to_date: datetime.date | None,
+) -> bool:
+    v_from_date = _get_from_date(versjon)
+    v_to_date = _get_to_date(versjon)
+
+    return (v_to_date is None or (from_date < v_to_date)) and (
+        to_date is None or (v_from_date <= to_date)
+    )
+
+
+def _build_change_graph(
+    classification: KlassClassification,
+    from_date: datetime.date,
+    to_date: datetime.date | None,
+) -> networkx.DiGraph:
+    versions_meta = filter(
+        functools.partial(
+            _dates_overlaps,
+            from_date=from_date,
+            to_date=to_date,
+        ),
+        classification.versions,
+    )
+
+    versions_ids: set[int] = set()
+    start_dates: dict[int, datetime.date] = {}
+    versions: list[KlassVersion] = []
+
+    for meta in versions_meta:
+        version_id = meta["version_id"]  # pyright: ignore[reportTypedDictNotRequiredAccess]
+        versions_ids.add(version_id)
+        start_dates[version_id] = _get_from_date(meta)
+        version = classification.get_version(version_id, language="nb")
+        versions.append(version)
+
+    graph = networkx.DiGraph()
+
+    for version1, version2 in pairwise(versions):
+        version1_kodes = cast(
+            set[str], set(item["code"] for item in version1.classificationItems)
+        )
+        version2_kodes = cast(
+            set[str], set(item["code"] for item in version2.classificationItems)
+        )
+
+        for code in version1_kodes & version2_kodes:
+            edge = (
+                _CodePoint(code, int(version1.version_id)),
+                _CodePoint(code, int(version2.version_id)),
+            )
+            graph.add_edge(*edge)
+
+    seen_change_tables_ides: set[int] = set()
+    change_tables: list[KlassCorrespondence] = []
+
+    for version in versions:
+        for change_table_meta in filter(
+            lambda v: v["sourceId"] in versions_ids and v["targetId"] in versions_ids,
+            version.correspondenceTables,
+        ):
+            change_table_id = change_table_meta["id"]
+            if change_table_id in seen_change_tables_ides:
+                continue
+
+            seen_change_tables_ides.add(change_table_id)
+            change_tables.append(version.get_correspondence(change_table_id))
+
+    for change_table in change_tables:
+        backwards = (
+            start_dates[change_table.sourceId] > start_dates[change_table.targetId]
+        )
+        for change in change_table.correspondence:
+            if change["sourceCode"] is None or change["targetCode"] is None:
+                continue
+            edge = (
+                _CodePoint(change["sourceCode"], change_table.sourceId),
+                _CodePoint(change["targetCode"], change_table.targetId),
+            )
+            if backwards:
+                edge = reversed(edge)
+            graph.add_edge(*edge)
+
+    return graph
+
+
+def get_changes_mapping(
+    classification: KlassClassification,
+    target_date: datetime.date | None = None,
+    from_date: datetime.date | None = None,
+    to_date: datetime.date | None = None,
+) -> pd.Series:
+    """Normalize codes from different time to a specified target date based on KLASS change tables.
+
+    Args:
+        classification: A KlassClassification object.
+        target_date: The date you want to normalize into.
+        from_date: The oldest date in the dataset
+        to_date: The newest date in the dataset
+    Returns:
+        A pandas series mapping existing to normalized codes, with existing codes as index labels, and normalized codes as values.
+        The index can contain duplicated labels
+    """
+    if target_date is None:
+        target_date = max(map(_get_from_date, classification.versions))
+
+    if from_date is None:
+        from_date = min(map(_get_from_date, classification.versions))
+
+    if target_date < from_date:
+        raise ValueError(
+            "Target date older than target date, or older than Klass classifcation"
+        )
+
+    graph = _build_change_graph(classification, from_date, to_date)
+
+    target_version = next(
+        filter(
+            functools.partial(
+                _dates_overlaps,
+                from_date=target_date,
+                to_date=target_date,
+            ),
+            classification.versions,
+        )
+    )
+
+    target_version_id = int(target_version["version_id"])
+
+    koorespondanser: set[tuple[str, str]] = set()
+    for target_code_point in filter(lambda n: n.version == target_version_id, graph):
+        older_code_points = networkx.dfs_preorder_nodes(graph, target_code_point)
+        newer_code_points = networkx.dfs_preorder_nodes(
+            graph.reverse(), target_code_point
+        )
+
+        koorespondanser.update(
+            (target_code_point.code, source_code_point.code)
+            for source_code_point in chain(older_code_points, newer_code_points)
+        )
+
+    values, index = zip(*koorespondanser, strict=True)
+    series = pd.Series(values, index=pd.Index(index, name="old_code"), name="new_code").sort_values().sort_index()
+    return series
