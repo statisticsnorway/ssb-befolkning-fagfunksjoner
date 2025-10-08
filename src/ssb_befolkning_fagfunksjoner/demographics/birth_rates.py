c5fb495afeb41876e6feda824ae7a9f792451161
ssb-grd <grd@ssb.no>
2025-10-08 13:01:47 +0200

    borth rate notes

---
+++ src/ssb_befolkning_fagfunksjoner/demographics/birth_rates.py
@@ -0,0 +1,19 @@
+# teller: fødte i en periode
+# nevner: middelfolkemengde i samme periode (start + slutt)/2
+
+# Lag rate for alle aldersgrupper
+# Sum av ratene er samlet fruktbarhetstall
+
+# På lavere nivå, aggreger på 5 års aldersgrupper 
+
+# tre inputs: dataframe start, dataframe slutt, fødte, tidsperiode, aggregeringsvariabel (geo, fødeland, landbakgrunn, utdanning, etc.)
+# - kjønn, alder 
+# valg av aldersinndelingsnivå (1år, 5år) - hvis på kommunenivå må det kanskje være 5år
+
+# perioder: måned, kvartal, år, 5-års intervall
+
+# SAS-kode: X:\320\1_oppdrag\Oppdragsgivere\2020\KMD\Andre KMD-oppdrag\20_0763 sft og sentralitet 2000-2019
+
+# validering: rater innafor forventet spenn
+# logging: min, max, median (viser outliers som kan hjelpe med valg av aggregering)
+
