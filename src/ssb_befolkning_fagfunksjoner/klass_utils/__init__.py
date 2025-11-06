036960a7158bb1b1a9691b7a0db3dfc2d453414e
Bjørn Lie Rapp <90246213+Bjoern-Rapp@users.noreply.github.com>
2025-11-06 17:11:59 +0100

    Export new function in __init__.

--- src/ssb_befolkning_fagfunksjoner/klass_utils/__init__.py
+++ src/ssb_befolkning_fagfunksjoner/klass_utils/__init__.py
@@ -1,5 +1,6 @@
 """Subpackage for interacting with KLASS specifically used in population statistics."""
 
 from .change_mapping import get_klass_change_mapping
+from .level_mapping import aggregate_codes
 
-__all__ = ["get_klass_change_mapping"]
+__all__ = ["aggregate_codes", "get_klass_change_mapping"]
