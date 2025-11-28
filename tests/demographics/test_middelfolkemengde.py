0b90b339b22a6889c449eab41f0ca13130e5bbc8
ssb-grd <grd@ssb.no>
2025-11-28 10:53:38 +0100

    remove incomplete test

--- tests/demographics/test_middelfolkemengde.py
+++ tests/demographics/test_middelfolkemengde.py
@@ -17,11 +17,3 @@ def n_df_slutt():
     return pd.DataFrame(
         {"alder": [31, 30], "kjoenn": ["2", "2"], "landsdel": ["B", "B"]}
     )
-
-
-def test_beregn_middelfolkemengde(
-    mocker: MockerFixture,
-    br_default: BirthRates,
-) -> None:
-    mock_gruppert = mocker.patch.object(br_default, "_tell_per_gruppe")
-    mock_gruppert.side_effect = [n_df_start, n_df_slutt]
