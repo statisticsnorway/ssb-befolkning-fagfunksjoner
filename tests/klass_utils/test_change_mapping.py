89ac9f71b29082e2fbb60b7ac556474bfb531556
Bjørn Lie Rapp <90246213+Bjoern-Rapp@users.noreply.github.com>
2026-01-20 12:44:36 +0100

    Extended tests.

--- tests/klass_utils/test_change_mapping.py
+++ tests/klass_utils/test_change_mapping.py
@@ -1,4 +1,5 @@
 import datetime
+from itertools import pairwise
 from unittest.mock import Mock
 
 import klass
@@ -21,9 +22,15 @@ def classification_mocker() -> Mock:
     classification_mocker = Mock(spec=klass.KlassClassification)
     versions_meta: list[VersionPartType] = [
         {
+            "name": "Kommuneinndeling 2026",
+            "version_id": 5,
+            "validFrom": "2026-01-01",
+        },
+        {
             "name": "Kommuneinndeling 2024",
             "version_id": 4,
             "validFrom": "2024-01-01",
+            "validTo": "2026-01-01",
         },
         {
             "name": "Kommuneinndeling 2023",
@@ -47,6 +54,14 @@ def classification_mocker() -> Mock:
     classification_mocker.versions = versions_meta
 
     change_meta: list[CorrespondenceTablesType] = [
+        {
+            "name": "Kommuneinndeling 2026 - Kommuneinndeling 2024",
+            "id": 4,
+            "source": "Kommuneinndeling 2026",
+            "sourceId": 5,
+            "target": "Kommuneinndeling 2024",
+            "targetId": 4,
+        },
         {
             "name": "Kommuneinndeling 2024 - Kommuneinndeling 2023",
             "id": 3,
@@ -73,17 +88,20 @@ def classification_mocker() -> Mock:
         },
     ]  # pyright: ignore[reportAssignmentType]
 
-    corr_meta: list[list[CorrespondenceTablesType]] = [
-        [change_meta[0]],
-        [change_meta[0], change_meta[1]],
-        [change_meta[1], change_meta[2]],
-        [change_meta[2]],
+    corr_meta: list[tuple[CorrespondenceTablesType, ...]] = [
+        (change_meta[0],),
+        *pairwise(change_meta),
+        (change_meta[-1],),
     ]
 
     versions_codes: list[list[dict[str, str]]] = [
+        [{"code": "3118"}, {"code": "3207"}, {"code": "3216"}],
         [
             {"code": "1508"},
             {"code": "1580"},
+            {"code": "3118"},
+            {"code": "3207"},
+            {"code": "3216"},
         ],
         [
             {"code": "1507"},
@@ -99,6 +117,13 @@ def classification_mocker() -> Mock:
     ]
 
     correspondence: list[list[dict[str, str]]] = [
+        [
+            {"sourceCode": "3118", "targetCode": "3118"},
+            {"sourceCode": "3207", "targetCode": "3118"},
+            {"sourceCode": "3207", "targetCode": "3207"},
+            {"sourceCode": "3216", "targetCode": "3118"},
+            {"sourceCode": "3216", "targetCode": "3216"},
+        ],
         [
             {"sourceCode": "1508", "targetCode": "1507"},
             {"sourceCode": "1580", "targetCode": "1507"},
@@ -122,6 +147,8 @@ def classification_mocker() -> Mock:
         mock.targetId = meta["targetId"]
         mock.correspondence = corr
 
+    change_table_mock.reverse()
+
     def _get_change_table(correspondence_id: int, *args, **kwargs) -> Mock:
         return change_table_mock[correspondence_id - 1]
 
@@ -130,12 +157,14 @@ def classification_mocker() -> Mock:
     ]
 
     for mock, v_id, meta, codes in zip(
-        version_mockers, range(4, 0, -1), corr_meta, versions_codes, strict=True
+        version_mockers, range(len(corr_meta), 0, -1), corr_meta, versions_codes, strict=True
     ):
         mock.correspondenceTables = meta
         mock.classificationItems = codes
         mock.version_id = v_id
         mock.get_correspondence = _get_change_table
+
+    version_mockers.reverse()
 
     def _get_version(version_id, *args, **kwargs):
         return version_mockers[version_id - 1]
@@ -148,6 +177,7 @@ def classification_mocker() -> Mock:
 cases = [
     (
         datetime.date(2024, 1, 1),
+        datetime.date(2025, 1, 1),
         pd.Series(
             [
                 "1508",
@@ -177,10 +207,12 @@ cases = [
     ),
     (
         datetime.date(2023, 1, 1),
+        datetime.date(2025, 1, 1),
         pd.Series(["1507"] * 6, index=["1504", "1507", "1508", "1523", "1529", "1580"]),
     ),
     (
         datetime.date(2019, 1, 1),
+        datetime.date(2025, 1, 1),
         pd.Series(
             [
                 "1504",
@@ -212,23 +244,55 @@ cases = [
             ],
         ),
     ),
+    (
+        datetime.date(2026, 1, 1),
+        None,
+        pd.Series(
+            [
+                "3118",
+                "3207",
+                "3216",
+            ],
+            index=[
+                "3118",
+                "3207",
+                "3216",
+            ],
+        ),
+    ),
 ]
 
 
-@pytest.mark.parametrize("target_date, expected", cases)
-def test_get_changes_mapping(classification_mocker, target_date, expected) -> None:
-    result = get_klass_change_mapping(classification_mocker, target_date=target_date)
+@pytest.mark.parametrize(("target_date", "to_date", "expected"), cases)
+def test_get_changes_mapping(
+    classification_mocker, target_date, to_date, expected
+) -> None:
+    result = get_klass_change_mapping(
+        classification_mocker, target_date=target_date, to_date=to_date
+    )
     pd.testing.assert_series_equal(expected, result, check_names=False)
 
 
-def test_label_graph(classification_mocker) -> None:
+cases = [
+    [(_CodePoint("1504", 1), _CodePoint("1507", 2)), _CessionType.CESSATION_WHOLE],
+    [(_CodePoint("1507", 2), _CodePoint("1507", 3)), _CessionType.NO_CESSION],
+    [
+        (_CodePoint("1507", 3), _CodePoint("1580", 4)),
+        _CessionType.CESSATION_PART_TO_NEW,
+    ],
+    [
+        (_CodePoint("3118", 4), _CodePoint("3207", 5)),
+        _CessionType.ADJUSTMENT_PART_TO_EXISTING,
+    ],
+    [(_CodePoint("3118", 4), _CodePoint("3118", 5)), _CessionType.BORDER_CHANGE],
+]
+
+
+@pytest.mark.parametrize(("edge", "expected"), cases)
+def test_label_graph(edge, expected, classification_mocker) -> None:
     result = _build_change_graph(
         classification_mocker, from_date=datetime.date(2019, 1, 1)
     )
     get_cession_type = networkx.get_edge_attributes(result, "cession_type")
 
-    edge = _CodePoint("1507", 3), _CodePoint("1580", 4)
-    assert get_cession_type[edge] == _CessionType.CESSATION_PART_TO_NEW
-
-    edge = _CodePoint("1504", 1), _CodePoint("1507", 2)
-    assert get_cession_type[edge] == _CessionType.CESSATION_WHOLE
+    assert get_cession_type[edge] == expected
