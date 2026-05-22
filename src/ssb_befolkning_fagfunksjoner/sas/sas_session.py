58bd649bbb6946d99b7255e545e2f12bd16ec0dc
ssb-grd <grd@ssb.no>
2026-05-22 20:19:10 +0200

    todo list

--- src/ssb_befolkning_fagfunksjoner/sas/sas_session.py
+++ src/ssb_befolkning_fagfunksjoner/sas/sas_session.py
@@ -95,3 +95,8 @@ def set_password() -> None:
             file.write(authstring)
     else:
         authfile_path.write_text(authstring)
+
+
+# TODO: 
+# 1. Er 'encodinga' sikker i en offentlig pakke?
+# 2. mypy klager og manglende stubs
